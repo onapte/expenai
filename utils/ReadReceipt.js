33e7ab025eebdef0b59d0449265dd59e797350ca
Onkar Apte <97520729+onapte@users.noreply.github.com>
2024-11-01 11:16:51 -0600

    Update ReadReceipt.js

--- utils/ReadReceipt.js
+++ utils/ReadReceipt.js
@@ -1,7 +1,7 @@
 const mindee = require('mindee');
 
 const getReceiptDetails = async (filestream) => {
-    const mindeeClient = new mindee.Client({apiKey: "c083ecb3dab1df04d3c06008b024b02b"});
+    const mindeeClient = new mindee.Client({apiKey: "<apiKey>"});
     const inputSource = mindeeClient.docFromStream(filestream);
 
     const apiResponse = mindeeClient.parse(
